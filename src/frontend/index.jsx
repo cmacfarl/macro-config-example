081cbde266acee2419d5f6b53af9abdd495771b6
Craig MacFarlane <camacfarl@gmail.com>
2024-06-17 10:12:51 -0700

    Minimal RadioGroup bug example

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,95 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text, Textfield, Select } from '@forge/react';
-import { invoke, view } from '@forge/bridge';
+import ForgeReconciler, { Text, RadioGroup, useConfig } from '@forge/react';
 
-const Tag = "macro-config-example: ";
+const defaultConfig = {color: 'purple'};
 
-function log(message) {
-  console.log(Tag + message);
-}
-
-const mockCouncilMembers = [
-  { label: "John Perry",   value: "1" },
-  { label: "Harry Wilson", value: "2" },
-  { label: "Jane Sagan",   value: "3" },
+const options = [
+  { name: 'color', value: 'red', label: 'Red' },
+  { name: 'color', value: 'blue', label: 'Blue' },
+  { name: 'color', value: 'yellow', label: 'Yellow' },
+  { name: 'color', value: 'green', label: 'Green' },
+  { name: 'color', value: 'black', label: 'Black' },
 ];
 
-function getCouncilMembers(city) {
-  /*
-   * In a real application this would make an api call to fetch council members for
-   * a given city.
-   */
-  if (city == "") {
-    log("No city no council");
-    return [];
-  } else {
-    log("City council for: " + city);
-    return mockCouncilMembers;
-  }
-}
-
-/*
- * React passes the parameter as an object.  If you call this as foo={bar}, 
- * then here, without {} you reference bar as city.foo.  The {} destructures the object
- * so you can reference bar as simply city.
- */
-function CouncilMembers({city}) {
-
-  const [councilMembers, setCouncilMembers] = useState([]);
-
-  useEffect(() => {
-    var cm = getCouncilMembers(city);
-    setCouncilMembers(cm);
-  }, [city]);
-
-  if (city != "") {
-    log(city + " council members: " + JSON.stringify(councilMembers));
-  }
-
-  return (
-    <>
-    { <Select 
-          isMulti
-          inputId="selected-councilpeople"
-          placeholder="Select desired council members..."
-          name="council"
-          label="Council Members"
-          options={councilMembers}/> }
-    </>
-  )
-}
-
 const Config = () => {
-  const [city, setCity] = useState("");
-
-  useEffect(() => {
-    view.getContext().then((ctx) => {
-        if (ctx?.extension.config?.city != undefined) {
-          setCity(ctx.extension.config.city);
-        }
-    })
-  }, []);
-
-  log("City: " + city);
   return (
     <>
-    <Textfield name="city" label="City" />
-    <CouncilMembers city={city}/> 
+    <RadioGroup options={options} value="red"/>
     </>
   );
 };
 
 const App = () => {
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, []);
+  const config = useConfig() || defaultConfig;
 
   return (
     <>
-      <Text>Hello world!</Text>
-      <Text>{data ? data : 'Loading...'}</Text>
+      <Text>Color: {config.color}</Text>
     </>
   );
 };
